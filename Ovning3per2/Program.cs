945321828ac74caadb2630eb519b000339435565
Judit Horvath <judith1horvath@outlook.com>
2021-06-23 14:50:23 +0200

    Break down Main

--- Ovning3per2/Program.cs
+++ Ovning3per2/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
 
 namespace Ovning3per2
 {
@@ -16,49 +17,20 @@ namespace Ovning3per2
 
             // 4. Skapa några djur (av olika typ) i din lista:
             List<Animal> animals = new List<Animal>();
-            
-            animals.Add(new Wolf(true, "BigWolf", 32.4, 8));
-            animals.Add(new Bird(true, "LittleBird", 1.4, 1));
-            animals.Add(new Swan(true,true, "WhiteSwan", 2, 3));
-            animals.Add(new Dog(true, "Fiffi", 23.2, 7));
-            animals.Add(new Worm(true, "OMG", 0.1, 1));
-            animals.Add(new Dog(false, "OlderFiffi", 41.3, 2));
-            animals.Add(new Wolfman(true,false, "CrazyMan", 81, 30));
-            animals.Add(new Hedgehog(false, "Thorny", 41.3, 2));
-            animals.Add(new Wolfman("James", 91, 60));
-
-
+             AddAnimals(animals);
 
 
 
             // 5. Skriv ut vilka djur som finns i listan med hjälp av en foreach-loop
             // 6. Anropa även Animals Sound() metod i foreach-loopen
-            Console.WriteLine("Animals in the list with sound:");
-            Console.WriteLine();
-            foreach (var animal in animals)
-            {
-                Console.WriteLine(animal);
-                animal.DoSound();
-                Console.WriteLine();
-            }
-            Console.WriteLine("----------------------");
+            PrintAnimalWithSound(animals);
 
 
 
 
 
             // 7.Gör en check i For-loopen ifall animal även är av typen Person, om den är det typ - casta till Person och anropa dess Talk() metod:
-            Console.WriteLine("Talking person:");
-            for (int i = 0; i < animals.Count; i++)
-            {      
-                if(animals[i] is IPerson)
-                {
-                    IPerson person = (IPerson)animals[i];
-                    Console.Write(person.ToString() + ". Person is saying: ");
-                    person.Talk();
-                }            
-            }
-            Console.WriteLine("----------------------");
+            CheckPersonAndTalk(animals);
 
 
 
@@ -66,9 +38,7 @@ namespace Ovning3per2
 
             //8. Skapa en lista för hundar:
             List<Dog> dogs = new List<Dog>();
-            dogs.Add(new Dog(true, "Admiral", 80.6, 3));
-            dogs.Add(new Dog(false, "Bartley", 13.2, 4));
-            dogs.Add(new Dog(true, "Chase", 31.7, 7));
+            AddDogs(dogs);
 
             // dogs.Add(new Horse(true));
             //9. F: Försök att lägga till en häst i listan av hundar. Varför fungerar inte det? -- List<Dog> innehåller bara hundar med hund property. Häst kan gå in i List<Horse> med egna properties.
@@ -79,12 +49,7 @@ namespace Ovning3per2
 
 
             // 11. Skriv ut samtliga Animals Stats() genom en foreach loop:
-            Console.WriteLine("Printing Animal Stats() method: ");
-            foreach (var animal in animals)
-            {               
-                Console.WriteLine(animal.Stats(animal.Name));
-            }       
-            Console.WriteLine("----------------------");
+            PrintAnimalWithAnimalSats(animals);
             // 13. F: Förklara vad det är som händer: foreach går igenom alla animal somm finns i listan, sedan använder Stats(string name) i Animal class. Eftersom Stats(string name) inte är Stats() finns det ingen override.
             // Det "call" Stats(string name) i Animal klassen istället Stats() som har override i olika animal klasserna. Om man "call" Stats() ska man får Stats() från animal klasserna pga override.
 
@@ -93,24 +58,13 @@ namespace Ovning3per2
 
 
             //Det är för Stats (från olika animals) för alla animals i listan:
-            Console.WriteLine("Printing different animal Stats() method: ");
-            foreach (var animal in animals)
-            {
-                Console.WriteLine(animal.Stats());
-            }
-            Console.WriteLine("----------------------");
+            PrintAnimalWithOwnSats(animals);
 
 
 
 
             // 14. Skriv ut Stats() metoden enbart för alla hundar genom en foreach på Animals:
-            Console.WriteLine("Stats() for Dogs");
-            foreach (var item in animals)
-            {
-                if (item is Dog)
-                    Console.WriteLine(item.Stats());
-            }
-            Console.WriteLine();
+            PrintDogslWithAnimalSats(animals);
 
 
 
@@ -133,6 +87,99 @@ namespace Ovning3per2
 
 
             // 18.Hitta ett sätt att skriva ut din nya metod för dog genom en foreach på Animals.
+            JumpingDog(animals);
+
+        }
+
+
+
+
+
+        public static void AddAnimals(List<Animal> animals) {
+            animals.Add(new Wolf(true, "BigWolf", 32.4, 8));
+            animals.Add(new Bird(true, "LittleBird", 1.4, 1));
+            animals.Add(new Swan(true, true, "WhiteSwan", 2, 3));
+            animals.Add(new Dog(true, "Fiffi", 23.2, 7));
+            animals.Add(new Worm(true, "OMG", 0.1, 1));
+            animals.Add(new Dog(false, "OlderFiffi", 41.3, 2));
+            animals.Add(new Wolfman(true, false, "CrazyMan", 81, 30));
+            animals.Add(new Hedgehog(false, "Thorny", 41.3, 2));
+            animals.Add(new Wolfman("James", 91, 60));
+        }
+
+
+        public static void PrintAnimalWithSound(List<Animal> animals)
+        {
+            Console.WriteLine("Animals in the list with sound:");
+            Console.WriteLine();
+            foreach (var animal in animals)
+            {
+                Console.WriteLine(animal);
+                animal.DoSound();
+                Console.WriteLine();
+            }
+            Console.WriteLine("----------------------");
+        }
+
+
+        public static void CheckPersonAndTalk(List<Animal> animals)
+        {
+            Console.WriteLine("Talking person:");
+            for (int i = 0; i < animals.Count; i++)
+            {
+                if (animals[i] is IPerson)
+                {
+                    IPerson person = (IPerson)animals[i];
+                    Console.Write(person.ToString() + ". Person is saying: ");
+                    person.Talk();
+                }
+            }
+            Console.WriteLine("----------------------");
+        }
+
+
+        public static void AddDogs(List<Dog> dogs)
+        {
+            dogs.Add(new Dog(true, "Admiral", 80.6, 3));
+            dogs.Add(new Dog(false, "Bartley", 13.2, 4));
+            dogs.Add(new Dog(true, "Chase", 31.7, 7));
+        }
+
+        //Printing Animal Stats() method
+        public static void PrintAnimalWithAnimalSats(List<Animal> animals)
+        {
+            Console.WriteLine("Printing Animal Stats() method: ");
+            foreach (var animal in animals)
+            {
+                Console.WriteLine(animal.Stats(animal.Name));
+            }
+            Console.WriteLine("----------------------");
+        }
+
+        public static void PrintAnimalWithOwnSats(List<Animal> animals)
+        {
+            Console.WriteLine("Printing different animal Stats() method: ");
+            foreach (var animal in animals)
+            {
+                Console.WriteLine(animal.Stats());
+            }
+            Console.WriteLine("----------------------");
+        }
+
+
+        public static void PrintDogslWithAnimalSats(List<Animal> animals)
+        {
+            Console.WriteLine("Stats() for Dogs");
+            foreach (var item in animals)
+            {
+                if (item is Dog)
+                    Console.WriteLine(item.Stats());
+            }
+            Console.WriteLine();
+        }
+
+        public static void JumpingDog(List<Animal> animals)
+        {
             Console.WriteLine("Jump() for Dogs");
             foreach (var item in animals)
             {
@@ -142,10 +189,9 @@ namespace Ovning3per2
                     Console.WriteLine(item.Jump());
                 }
             }
-            Console.WriteLine();   
-
+            Console.WriteLine();
         }
 
-    }
+        }
 }
 
